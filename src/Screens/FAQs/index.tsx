76edc147d31b14917a39f5f553ab558f38338948
FenaliEnacton <77476729+FenaliEnacton@users.noreply.github.com>
2024-03-19 17:39:10 +0530

    feat: faqs converted to functional

--- src/Screens/FAQs/index.tsx
+++ src/Screens/FAQs/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { connect } from 'react-redux';
 import HTMLView from 'react-native-htmlview';
@@ -24,89 +24,82 @@ const mapStateToProps = ({ params }) => {
   };
 };
 
-class FAQs extends Component<any> {
-  state = {
-    par_index: null,
-    que_index: null,
-  };
+const FAQs = props => {
+  const [parIndex, setParIndex] = useState(null);
+  const [queIndex, setQueIndex] = useState(null);
 
-  componentDidMount() {
-    this.props.request_faqs();
-  }
+  useEffect(() => {
+    props.request_faqs();
+  }, []);
 
-  toggle = (par_index, child_index) => {
-    if (
-      this.state.par_index === par_index &&
-      this.state.que_index === child_index
-    ) {
-      this.setState({ par_index: null, que_index: null });
+  const toggle = (par_index, child_index) => {
+    if (parIndex === par_index && queIndex === child_index) {
+      setQueIndex(null);
+      setParIndex(null);
     } else {
-      this.setState({ par_index, que_index: child_index });
+      setQueIndex(child_index);
+      setParIndex(par_index);
     }
   };
 
-  render() {
-    const { faqs_info } = this.props;
-    const { par_index, que_index } = this.state;
-    return (
-      <Container>
-        <Header>
-          <Header.Left>
-            <HeaderBackButton onPress={() => this.props.navigation.goBack()} />
-          </Header.Left>
-          <Header.Title>
-            <Text style={styles.headerTitle}>{translate('faqs')}</Text>
-          </Header.Title>
-          <Header.Right />
-        </Header>
-        <ScrollContent>
-          {faqs_info.map((e, index) => {
-            return (
-              <View style={styles.screen_content}>
-                <Text style={styles.sectionTitle}>{e.name}</Text>
-                {e.faq.map((q, i) => {
-                  return (
-                    <View style={styles.faq_card}>
-                      <TouchableOpacity
-                        style={styles.faq_card_head}
-                        onPress={() => this.toggle(index, i)}>
-                        <Text style={styles.faq_title}>{q.title}</Text>
-                        <Icons.AntDesign
-                          name={
-                            par_index === index && que_index === i
-                              ? 'up'
-                              : 'down'
-                          }
-                          color={Theme.COLORS.primary}
-                          size={14}
-                        />
-                      </TouchableOpacity>
-                      {par_index === index && que_index === i ? (
-                        <HTMLView
-                          style={styles.faq_content}
-                          value={q.content}
-                          stylesheet={StyleSheet.create({
-                            ...Theme.fontStyles.html_view_txtStyles,
-                          })}
-                        />
-                      ) : null}
-                    </View>
-                  );
-                })}
-              </View>
-            );
-          })}
-          <GradientFooter
-            button_title={translate('contact_us')}
-            main_title={''}
-            sub_title={translate('faq_gr_sub_title')}
-            image={AppImages.gr_faq_img}
-            buttonClick={() => this.props.navigation.navigate('ContactUs')}
-          />
-        </ScrollContent>
-      </Container>
-    );
-  }
-}
+  const { faqs_info } = props;
+
+  return (
+    <Container>
+      <Header>
+        <Header.Left>
+          <HeaderBackButton onPress={() => props.navigation.goBack()} />
+        </Header.Left>
+        <Header.Title>
+          <Text style={styles.headerTitle}>{translate('faqs')}</Text>
+        </Header.Title>
+        <Header.Right />
+      </Header>
+      <ScrollContent>
+        {faqs_info.map((e, index) => {
+          return (
+            <View style={styles.screen_content}>
+              <Text style={styles.sectionTitle}>{e.name}</Text>
+              {e.faq.map((q, i) => {
+                return (
+                  <View style={styles.faq_card}>
+                    <TouchableOpacity
+                      style={styles.faq_card_head}
+                      onPress={() => toggle(index, i)}>
+                      <Text style={styles.faq_title}>{q.title}</Text>
+                      <Icons.AntDesign
+                        name={
+                          parIndex === index && queIndex === i ? 'up' : 'down'
+                        }
+                        color={Theme.COLORS.primary}
+                        size={14}
+                      />
+                    </TouchableOpacity>
+                    {parIndex === index && queIndex === i ? (
+                      <HTMLView
+                        style={styles.faq_content}
+                        value={q.content}
+                        stylesheet={StyleSheet.create({
+                          ...Theme.fontStyles.html_view_txtStyles,
+                        })}
+                      />
+                    ) : null}
+                  </View>
+                );
+              })}
+            </View>
+          );
+        })}
+        <GradientFooter
+          button_title={translate('contact_us')}
+          main_title={''}
+          sub_title={translate('faq_gr_sub_title')}
+          image={AppImages.gr_faq_img}
+          buttonClick={() => props.navigation.navigate('ContactUs')}
+        />
+      </ScrollContent>
+    </Container>
+  );
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(FAQs);
