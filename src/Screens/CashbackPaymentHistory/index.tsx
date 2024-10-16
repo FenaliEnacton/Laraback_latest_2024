a5e47e6e58b43f88da03f565d9b71312445985a3
FenaliEnacton <fhkhajanchiwala@enacton.com>
2024-10-16 11:30:46 +0530

    feat: cashback payment converted to functional component

--- src/Screens/CashbackPaymentHistory/index.tsx
+++ src/Screens/CashbackPaymentHistory/index.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { View, Text, FlatList, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
 import Icon from '@/Assets/icons';
@@ -26,40 +26,26 @@ import BottomModal from '@/Components/Core/BottomModal';
 import CloseButton from '@/Components/Core/CloseButton';
 
 const NAV_LIST_1 = get_user_internal_nav_list([6666]);
-const monthModal: any = React.createRef();
-const mapDispatchToProps = {
+
+const CashbackPaymentHistory = ({
+  user_payment_list,
+  current_selected_month,
+  loading,
+  payment_months_data,
   request_user_payment_list,
   request_user_payment_email_verify,
-};
+  navigation,
+}) => {
+  const [showMonthPicker, setShowMonthPicker] = useState(false);
+  const monthModal = useRef<any>(null);
 
-const mapStateToProps = ({ params }) => {
-  return {
-    user_payment_list: params.user_payment_list || [],
-    current_selected_month:
-      params.user_payment_month || dayjs().format('YYYYMM'),
-    loading: params.loading,
-    payment_months_data: params.user_payment_summary
-      ? params.user_payment_summary.months
-        ? user_activity_months(params.user_payment_summary.months)
-        : {}
-      : {},
-  };
-};
-
-class CashbackPaymentHistory extends Component<any> {
-  state = {
-    showMonthPicker: false,
-  };
-
-  componentDidMount() {
-    if (this.props.payment_months_data?.recent_month) {
-      this.props.request_user_payment_list(
-        this.props.payment_months_data?.recent_month,
-      );
+  useEffect(() => {
+    if (payment_months_data?.recent_month) {
+      request_user_payment_list(payment_months_data?.recent_month);
     }
-  }
+  }, []);
 
-  render_clicks = ({ item, index }) => {
+  const render_clicks = ({ item, index }) => {
     return (
       <View style={styles.tabCard} key={index + item.id.toString()}>
         <View style={styles.storeInfoCard}>
@@ -94,8 +80,7 @@ class CashbackPaymentHistory extends Component<any> {
               styles.statusBox,
               { backgroundColor: Theme.get_status_light_color(item.status) },
             ]}>
-            <Text style={[styles.status]}>
-              {''}
+            <Text style={styles.status}>
               {item.user_cashback_id
                 ? translate('tracked')
                 : translate('clicked')}
@@ -105,9 +90,7 @@ class CashbackPaymentHistory extends Component<any> {
           {item.status === 'created' && item.verified_at == null ? (
             <TouchableOpacity
               style={styles.resend_btn}
-              onPress={() =>
-                this.props.request_user_payment_email_verify(item.id)
-              }>
+              onPress={() => request_user_payment_email_verify(item.id)}>
               <Text style={styles.resend_txt}>{translate('resend')}</Text>
             </TouchableOpacity>
           ) : null}
@@ -116,32 +99,30 @@ class CashbackPaymentHistory extends Component<any> {
     );
   };
 
-  handle_month_selection = month_id => {
-    this.props.request_user_payment_list(month_id);
+  const handle_month_selection = month_id => {
+    request_user_payment_list(month_id);
     monthModal.current.props.onRequestClose();
   };
-  addEmptyCard = () => {
-    return <View style={{ height: 80 }} />;
-  };
-  renderMonths = ({ item, index }) => {
+
+  const renderMonths = ({ item, index }) => {
     return (
       <TouchableOpacity
         style={[
           styles.monthTab,
           {
             backgroundColor:
-              this.props.current_selected_month === item.month_id
+              current_selected_month === item.month_id
                 ? Theme.COLORS.primary
                 : Theme.COLORS.white,
           },
         ]}
-        onPress={() => this.handle_month_selection(item.month_id)}>
+        onPress={() => handle_month_selection(item.month_id)}>
         <Text
           style={[
             styles.monthText,
             {
               color:
-                this.props.current_selected_month === item.month_id
+                current_selected_month === item.month_id
                   ? Theme.COLORS.white
                   : Theme.COLORS.blackText,
             },
@@ -152,107 +133,106 @@ class CashbackPaymentHistory extends Component<any> {
     );
   };
 
-  render() {
-    const {
-      payment_months_data,
-      current_selected_month,
-      user_payment_list,
-      loading,
-    } = this.props;
-    const loader_arr = [1, 2, 3, 4];
-    return (
-      <Container>
-        <Header>
-          <Header.Left>
-            <HeaderBackButton onPress={() => this.props.navigation.goBack()} />
-          </Header.Left>
-          <Header.Title>
-            <Text style={styles.headerTitle} numberOfLines={1}>
-              {translate('cashback_payment_history')}
-            </Text>
-          </Header.Title>
-          <Header.Right />
-        </Header>
-        <View style={styles.content}>
-          {/* <Text style={styles.screen_title}>
+  const addEmptyCard = () => <View style={{ height: 80 }} />;
+
+  const loader_arr = [1, 2, 3, 4];
+
+  return (
+    <Container>
+      <Header>
+        <Header.Left>
+          <HeaderBackButton onPress={() => navigation.goBack()} />
+        </Header.Left>
+        <Header.Title>
+          <Text style={styles.headerTitle} numberOfLines={1}>
             {translate('cashback_payment_history')}
-          </Text> */}
-
-          <View style={styles.navCard}>
-            <ListHeader
-              month={
-                user_payment_list.length
-                  ? dayjs(current_selected_month).format('MMM-YYYY')
-                  : null
-              }
-              title={translate('cashback_payment_history')}
-              onPress={() => this.setState({ showMonthPicker: true })}
-            />
-          </View>
-          <View style={{ marginTop: 5 }}>
-            <FlatList
-              data={user_payment_list}
-              extraData={this.props}
-              showsVerticalScrollIndicator={false}
-              renderItem={this.render_clicks}
-              keyExtractor={(item, index) => index.toString()}
-              style={styles.list}
-              ListFooterComponent={this.addEmptyCard}
-            />
-            {loading &&
-              loader_arr.map((item, index) => {
-                return <TabLoader key={item.toString() + index} />;
-              })}
-            {!loading && !user_payment_list.length && <EmptyListView />}
-          </View>
-          {/* <ActivityNavigationList
-            list={NAV_LIST_1}
-            navigation={this.props.navigation}
-          /> */}
-        </View>
-        <BlurNavBar>
-          <NavigationList
-            list={NAV_LIST_1}
-            navigation={this.props.navigation}
-            style={styles.navListStyle}
-            containerStyle={{
-              // marginVertical: 20,
-              alignItems: 'center',
-              marginTop: 0,
-            }}
-            numberOfLines={1}
-            textStyle={styles.routeText}
+          </Text>
+        </Header.Title>
+        <Header.Right />
+      </Header>
+      <View style={styles.content}>
+        <View style={styles.navCard}>
+          <ListHeader
+            month={
+              user_payment_list.length
+                ? dayjs(current_selected_month).format('MMM-YYYY')
+                : null
+            }
+            title={translate('cashback_payment_history')}
+            onPress={() => setShowMonthPicker(true)}
           />
-        </BlurNavBar>
-        <BottomModal
-          ref={monthModal}
-          bottomModalShow={this.state.showMonthPicker}
-          setBottomModalVisibleFalse={() =>
-            this.setState({ showMonthPicker: false })
-          }>
-          <>
-            <View style={styles.modal_top_notch} />
-
-            <Text style={styles.title}>{translate('select_month')}</Text>
-            <FlatList
-              style={styles.modalList}
-              data={payment_months_data.revised_months}
-              keyExtractor={(item, index) => index.toString()}
-              extraData={this.props}
-              renderItem={this.renderMonths}
+        </View>
+        <View style={{ marginTop: 5 }}>
+          <FlatList
+            data={user_payment_list}
+            extraData={user_payment_list}
+            showsVerticalScrollIndicator={false}
+            renderItem={render_clicks}
+            keyExtractor={(item, index) => index.toString()}
+            style={styles.list}
+            ListFooterComponent={addEmptyCard}
+          />
+          {loading &&
+            loader_arr.map((item, index) => (
+              <TabLoader key={item.toString() + index} />
+            ))}
+          {!loading && !user_payment_list.length && <EmptyListView />}
+        </View>
+      </View>
+      <BlurNavBar>
+        <NavigationList
+          list={NAV_LIST_1}
+          navigation={navigation}
+          style={styles.navListStyle}
+          containerStyle={{
+            alignItems: 'center',
+            marginTop: 0,
+          }}
+          numberOfLines={1}
+          textStyle={styles.routeText}
+        />
+      </BlurNavBar>
+      <BottomModal
+        ref={monthModal}
+        bottomModalShow={showMonthPicker}
+        setBottomModalVisibleFalse={() => setShowMonthPicker(false)}>
+        <>
+          <View style={styles.modal_top_notch} />
+          <Text style={styles.title}>{translate('select_month')}</Text>
+          <FlatList
+            style={styles.modalList}
+            data={payment_months_data.revised_months}
+            keyExtractor={(item, index) => index.toString()}
+            extraData={current_selected_month}
+            renderItem={renderMonths}
+          />
+          <View style={styles.btnBar}>
+            <CloseButton
+              btnStyle={styles.closeBtn}
+              onPress={() => monthModal.current.props.onRequestClose()}
             />
-            <View style={styles.btnBar}>
-              <CloseButton
-                btnStyle={styles.closeBtn}
-                onPress={() => monthModal.current.props.onRequestClose()}
-              />
-            </View>
-          </>
-        </BottomModal>
-      </Container>
-    );
-  }
-}
+          </View>
+        </>
+      </BottomModal>
+    </Container>
+  );
+};
+
+const mapStateToProps = ({ params }) => ({
+  user_payment_list: params.user_payment_list || [],
+  current_selected_month: params.user_payment_month || dayjs().format('YYYYMM'),
+  loading: params.loading,
+  payment_months_data: params.user_payment_summary
+    ? params.user_payment_summary.months
+      ? user_activity_months(params.user_payment_summary.months)
+      : {}
+    : {},
+});
+
+const mapDispatchToProps = {
+  request_user_payment_list,
+  request_user_payment_email_verify,
+};
 
 export default connect(
   mapStateToProps,
