fb4b368111470be30a444ffb35e26b09b57ce2c3
FenaliEnacton <fhkhajanchiwala@enacton.com>
2024-11-29 11:03:09 +0530

    feat: favorite screen converted to functional component

--- src/Screens/Favorites/index.tsx
+++ src/Screens/Favorites/index.tsx
@@ -1,77 +1,75 @@
-import React, { Component } from 'react';
-import { View, Text, FlatList } from 'react-native';
-import { connect } from 'react-redux';
-import { translate } from '@/translations';
-import TopStoreCard from '@/Components/Generic/TopStoreCard';
 import Container from '@/Components/Core/Container';
+import DrawerMenu from '@/Components/Core/DrawerMenu';
 import Header from '@/Components/Core/Header/Header';
 import HeaderLeft from '@/Components/Core/Header/HeaderLeft';
 import HeaderRight from '@/Components/Core/Header/HeaderRight';
 import HeaderTitle from '@/Components/Core/Header/HeaderTitle';
 import HeaderBackButton from '@/Components/Core/HeaderBackButton';
+import LogoutModal from '@/Components/Core/LogoutModal';
 import EmptyListView from '@/Components/Generic/EmptyListView';
+import TopStoreCard from '@/Components/Generic/TopStoreCard';
+import useUserFav from '@/Hooks/Api/use-user-fav';
+import { translate } from '@/translations';
+import React, { useState } from 'react';
+import { FlatList, Text, View } from 'react-native';
 import styles from './style';
-import { get_fav_stores } from '@/Redux/USER_REDUX/Selectors';
-import { Theme } from '@/Assets/Theme';
 
-const mapDispatchToProps = {};
+const Favorites = props => {
+  const { userFavData } = useUserFav();
+  const [showDrawer, setShowDrawer] = useState(false);
+  const [logoutShow, setLogoutShow] = useState(false);
 
-const mapStateToProps = ({ params }) => {
-  return {
-    fav_stores: get_fav_stores(params.user_fav_data) || [],
-  };
-};
-
-class Favorites extends Component<any> {
-  renderStores = ({ item, index }) => {
+  const render_empty = () => {
     return (
-      <TopStoreCard store={item} bg_color={Theme.get_bg_color(index, 2)} />
-    );
-  };
-
-  render_empty = () => {
-    return (
-      <>
+      <View>
         <EmptyListView message={translate('no_fav_found')} />
         <Text
           style={styles.store_nav}
-          onPress={() => this.props.navigation.navigate('AllStores')}>
+          onPress={() => props.navigation.navigate('AllStores')}>
           {translate('click_here_to_add')}
         </Text>
-      </>
+      </View>
     );
   };
-  render() {
-    const { fav_stores } = this.props;
-    return (
-      <Container>
-        <Header>
-          <HeaderLeft>
-            <HeaderBackButton onPress={() => this.props.navigation.goBack()} />
-          </HeaderLeft>
-          <HeaderTitle>
-            <Text style={styles.headerTitle}>
-              {translate('favorites_stores')}
-            </Text>
-          </HeaderTitle>
-          <HeaderRight />
-        </Header>
-        <View style={styles.content}>
-          <FlatList
-            keyExtractor={(item, index) => index.toString()}
-            data={fav_stores}
-            style={styles.list}
-            extraData={this.props}
-            columnWrapperStyle={styles.row}
-            numColumns={2}
-            showsVerticalScrollIndicator={false}
-            ListEmptyComponent={() => this.render_empty()}
-            renderItem={this.renderStores}
-          />
-        </View>
-      </Container>
-    );
-  }
-}
-
-export default connect(mapStateToProps, mapDispatchToProps)(Favorites);
+  const renderStores = ({ item, index }) => {
+    return <TopStoreCard store={item} isFavorite={true} />;
+  };
+  return (
+    <Container>
+      <Header>
+        <HeaderLeft>
+          <HeaderBackButton onPress={() => props.navigation.goBack()} />
+        </HeaderLeft>
+        <HeaderTitle>
+          <Text style={styles.headerTitle}>
+            {translate('favorites_stores')}
+          </Text>
+        </HeaderTitle>
+        <HeaderRight />
+      </Header>
+      <View style={styles.content}>
+        <FlatList
+          keyExtractor={(item, index) => index.toString()}
+          data={userFavData?.['0']?.['fav_data']}
+          style={styles.list}
+          columnWrapperStyle={styles.row}
+          numColumns={2}
+          showsVerticalScrollIndicator={false}
+          ListEmptyComponent={() => render_empty()}
+          renderItem={renderStores}
+        />
+      </View>
+      <DrawerMenu
+        drawerShow={showDrawer}
+        navigation={props.navigation}
+        setDrawerVisibleFalse={() => setShowDrawer(false)}
+        show_log_out={() => setLogoutShow(true)}
+      />
+      <LogoutModal
+        onRequestClose={() => setLogoutShow(false)}
+        visible={logoutShow}
+      />
+    </Container>
+  );
+};
+export default Favorites;
