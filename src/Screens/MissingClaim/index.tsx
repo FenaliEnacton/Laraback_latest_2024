4870dcf4c5321d58c2674ac4a5122d55c981f9a0
FenaliEnacton <fhkhajanchiwala@enacton.com>
2024-11-29 11:19:47 +0530

    feat: missing claim convrted to fun

--- src/Screens/MissingClaim/index.tsx
+++ src/Screens/MissingClaim/index.tsx
@@ -4,41 +4,28 @@ import Icons from '@/Assets/icons';
 import Container from '@/Components/Core/Container';
 import Header from '@/Components/Core/Header/Header';
 import HeaderBackButton from '@/Components/Core/HeaderBackButton';
-import BlurNavBar from '@/Components/Generic/BlurNavBar';
 import EmptyListView from '@/Components/Generic/EmptyListView';
 import NavigationList from '@/Components/User/NavigationList';
 import TabLoader from '@/Components/User/TabLoader';
-import {
-  request_claim_info,
-  request_user_claim_list,
-} from '@/Redux/USER_REDUX/Actions/userClaimActions';
+import useUserClaim from '@/Hooks/Api/use-user-claim';
 import { Config } from '@/react-native-config';
 import { translate } from '@/translations';
 import dayjs from 'dayjs';
-import React, { Component } from 'react';
-import { FlatList, Image, Text, View } from 'react-native';
-import { connect } from 'react-redux';
+import React, { useEffect } from 'react';
+import { Dimensions, FlatList, Image, Text, View } from 'react-native';
 import styles from './style';
 const NAV_LIST = get_user_internal_nav_list([7777]);
+const windowWidth = Dimensions.get('window').width;
+const MissingClaim = props => {
+  const { request_user_claim_list, loadingUserClaimList, userClaimList } =
+    useUserClaim();
 
-const mapDispatchToProps = {
-  request_user_claim_list,
-  request_claim_info,
-};
+  useEffect(() => {
+    request_user_claim_list();
+  }, []);
+  const loader_arr = [1, 2, 3, 4];
 
-const mapStateToProps = ({ params }) => {
-  return {
-    claims: params.user_claim_list || [],
-    loading: params.loading,
-  };
-};
-
-class MissingClaim extends Component<any> {
-  componentDidMount() {
-    this.props.request_user_claim_list();
-  }
-
-  render_claims = ({ item, index }) => {
+  const render_claims = ({ item, index }) => {
     return (
       <View style={styles.tabCard} key={index + item.id.toString()}>
         <View style={styles.storeInfoCard}>
@@ -80,68 +67,59 @@ class MissingClaim extends Component<any> {
           ]}>
           <Text style={[styles.status]}>
             {''}
-            {item.user_cashback_id
-              ? translate('tracked')
-              : translate('clicked')}
+            {item.status}
           </Text>
         </View>
       </View>
     );
   };
-  addEmptyCard = () => {
-    return <View style={{ height: 80 }} />;
-  };
-  render() {
-    const { claims, loading } = this.props;
-    const loader_arr = [1, 2, 3, 4];
-    return (
-      <Container>
-        <Header>
-          <Header.Left>
-            <HeaderBackButton onPress={() => this.props.navigation.goBack()} />
-          </Header.Left>
-          <Header.Title>
-            <Text numberOfLines={1} style={styles.headerTitle}>
-              {translate('missing_cashback_claims')}
-            </Text>
-          </Header.Title>
-          <Header.Right />
-        </Header>
-        <View style={{ marginTop: 55 }}>
-          <View>
-            {!loading ? (
-              <FlatList
-                showsVerticalScrollIndicator={false}
-                extraData={this.props}
-                data={claims}
-                renderItem={this.render_claims}
-                keyExtractor={(item, index) => index.toString()}
-                // style={styles.list}
-                ListFooterComponent={this.addEmptyCard}
-              />
-            ) : (
-              loader_arr.map((item, index) => {
-                return <TabLoader key={item.toString() + index} />;
-              })
-            )}
-            {!loading && !claims.length && <EmptyListView />}
-          </View>
-        </View>
-        <BlurNavBar>
-          <NavigationList
-            list={NAV_LIST}
-            navigation={this.props.navigation}
-            style={styles.navListStyle}
-            numberColumn={1}
-            containerStyle={{
-              marginTop: 0,
-            }}
-            textStyle={styles.routeText}
+  // const addEmptyCard = () => {
+  //   return <View style={{ height: 80 }} />;
+  // };
+  return (
+    <Container>
+      <Header>
+        <Header.Left>
+          <HeaderBackButton
+            btnStyle={{ backgroundColor: Theme.COLORS.primary }}
+            onPress={() => props.navigation.goBack()}
           />
-        </BlurNavBar>
-      </Container>
-    );
-  }
-}
+        </Header.Left>
+        <Header.Title>
+          <Text numberOfLines={1} style={styles.headerTitle}>
+            {translate('missing_cashback_claims')}
+          </Text>
+        </Header.Title>
+        <Header.Right />
+      </Header>
+      <View style={{ marginTop: 55, width: windowWidth }}>
+        {!loadingUserClaimList ? (
+          <FlatList
+            showsVerticalScrollIndicator={false}
+            data={userClaimList}
+            renderItem={render_claims}
+            keyExtractor={(item, index) => index.toString()}
+            ListEmptyComponent={<EmptyListView />}
+          />
+        ) : (
+          loader_arr.map((item, index) => {
+            return <TabLoader key={item.toString() + index} />;
+          })
+        )}
 
-export default connect(mapStateToProps, mapDispatchToProps)(MissingClaim);
+        <NavigationList
+          list={NAV_LIST}
+          navigation={props.navigation}
+          style={styles.navListStyle}
+          numberColumn={1}
+          containerStyle={{
+            marginTop: 0,
+          }}
+          textStyle={styles.routeText}
+        />
+      </View>
+    </Container>
+  );
+};
+
+export default MissingClaim;
