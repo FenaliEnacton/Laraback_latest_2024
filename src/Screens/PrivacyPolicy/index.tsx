428151ad8325aeec23f04c16b13c2a6243b4e7cd
FenaliEnacton <fhkhajanchiwala@enacton.com>
2024-11-29 11:23:15 +0530

    feat: privacy policy converted to func

--- src/Screens/PrivacyPolicy/index.tsx
+++ src/Screens/PrivacyPolicy/index.tsx
@@ -4,51 +4,50 @@ import ScrollContent from '@/Components/Core/Content/scrollContent';
 import Header from '@/Components/Core/Header/Header';
 import HeaderBackButton from '@/Components/Core/HeaderBackButton';
 import Loader from '@/Components/Core/Loader';
+import usePublicData from '@/Hooks/Api/use-public-data';
 import { translate } from '@/translations';
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text } from 'react-native';
 import HTMLView from 'react-native-htmlview';
-import { connect } from 'react-redux';
 import styles from './style';
 
-const mapDispatchToProps = {};
-
-const mapStateToProps = ({ params }) => {
-  return {
-    privacy_policy_data: params.app_info_data?.privacy || {},
-    loading: params.loading,
-  };
-};
-
-class PrivacyPolicy extends Component<any> {
-  render() {
-    const { privacy_policy_data, loading } = this.props;
-    return (
-      <Container>
-        <Header>
-          <Header.Left>
-            <HeaderBackButton onPress={() => this.props.navigation.goBack()} />
-          </Header.Left>
-          <Header.Title>
-            <Text style={styles.headerTitle}>
-              {translate('privacy_policy')}
-            </Text>
-          </Header.Title>
-          <Header.Right />
-        </Header>
-        <ScrollContent>
+const PrivacyPolicy = props => {
+  const { request_privacy_terms, privacyTerms, loadingPrivacyTerms }: any =
+    usePublicData();
+  useEffect(() => {
+    if (Object.keys(privacyTerms).length === 0) request_privacy_terms();
+  }, []);
+  return (
+    <Container>
+      <Header>
+        <Header.Left>
+          <HeaderBackButton onPress={() => props.navigation.goBack()} />
+        </Header.Left>
+        <Header.Title>
+          <Text style={[styles.headerTitle]}>
+            {translate('privacy_policy')}
+          </Text>
+        </Header.Title>
+        <Header.Right />
+      </Header>
+      <ScrollContent>
+        {privacyTerms?.privacy?.footer_content && (
           <HTMLView
             style={styles.terms_content}
-            value={privacy_policy_data.footer_content}
+            value={privacyTerms?.privacy?.footer_content}
+            textComponentProps={{
+              style: { color: Theme.COLORS.black },
+            }}
             stylesheet={StyleSheet.create({
               ...Theme.fontStyles.html_view_txtStyles,
+              color: Theme.COLORS.black,
             })}
           />
-        </ScrollContent>
-        <Loader show={loading} />
-      </Container>
-    );
-  }
-}
+        )}
+      </ScrollContent>
+      <Loader show={loadingPrivacyTerms} />
+    </Container>
+  );
+};
 
-export default connect(mapStateToProps, mapDispatchToProps)(PrivacyPolicy);
+export default PrivacyPolicy;
